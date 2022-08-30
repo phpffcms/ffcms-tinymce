075bb43dc47d0382a8e6ea9db5234a17422b295d
Piatinskii M <zenn1989@gmail.com>
2022-08-30 14:03:06 +0300

    allow visual designing some elements

--- src/Apps/View/Admin/default/widgets/tinymce/full.php
+++ src/Apps/View/Admin/default/widgets/tinymce/full.php
@@ -2,6 +2,7 @@
 /** @var \Ffcms\Templex\Template\Template $this */
 ?>
 
+<script src="<?= \App::$Alias->scriptUrl ?>/vendor/phpffcms/ffcms-assets/node_modules/xss/dist/xss.min.js"></script>
 <script src="<?= \App::$Alias->scriptUrl ?>/vendor/tinymce/tinymce/tinymce.min.js"></script>
 <script src="<?= \App::$Alias->scriptUrl ?>/vendor/phpffcms/ffcms-assets/standalone/tinymce-codeeditor/ce.js"></script>
 
@@ -21,14 +22,14 @@
         plugins: [
             'advlist autolink lists link image imagetools charmap print preview anchor',
             'searchreplace visualblocks code',
-            'insertdatetime media table paste wordcount pagebreak codesample codeeditor'
+            'insertdatetime media table paste wordcount pagebreak codesample codeeditor paste'
         ],
-        toolbar: 'formatselect | insert | bold italic subscript superscript forecolor backcolor blockquote | removeformat | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | pagebreak codeeditor',
+        toolbar: 'paste | formatselect | insert | bold italic subscript superscript forecolor backcolor blockquote | removeformat | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | pagebreak codeeditor',
         image_advtab: true,
         visualblocks_default_state: true,
         end_container_on_empty_block: true,
         default_link_target: "_blank",
-        paste_as_text: true,
+        //paste_as_text: true,
         extended_valid_elements: 'i[class|id],span[class,id]',
         pagebreak_separator: "<div style=\"page-break-after: always;\">&nbsp;</div>",
         pagebreak_split_block: true,
@@ -62,6 +63,39 @@
             e.on('change', function(){
                 e.save();
             });
+        },
+        paste_preprocess: function(plugin, args) {
+            let htm = args.content;
+            html = filterXSS(htm, {
+                whiteList: {
+                    a: ['href', 'title', 'target'],
+                    p: [],
+                    strong: [],
+                    b: [],
+                    table: [],
+                    tr: ['colspan'],
+                    td: ['colspan'],
+                    thead: [],
+                    tbody: []
+                },
+                stripIgnoreTag: true,
+                stripIgnoreTagBody: ['script'],
+                onTag: function(tag, html, options) {
+                    if (tag === 'table') {
+                        res = html;
+                        if (options.isClosing === true) {
+                            res = "</div>" + res;
+                        } else {
+                            res = res.replace('>', ' class="table table-bordered">');
+                            res = '<div class="table-responsive">' + res;
+                        }
+                        return res;
+                    }
+                }
+            });
+
+            //console.log(html);
+            args.content = html;
         }
     });
 </script>
